0fea244c8197086adb33e7bfd5d4c7427db77751
Thomas Bos <tbos@infiniot.nl>
2024-12-26 23:30:30 +0100

    Add fall out now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
  import HeaderImage from "$lib/images/brick-wall-bw.jpg"
+ import FallArt from "$lib/images/fall.png"
 </script>
 
 <main>
@@ -9,15 +10,42 @@
     </div>
 
     <div id="nav-bar" class="font-nav text-[35px] w-full sticky top-0 bg-white/20 backdrop-blur-sm grid grid-cols-4 content-center place-items-center">
-        <div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white">HOME</div>
-        <div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white">BIO</div>
-        <div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white">CONTENT</div>
-        <div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white">LINKS</div>
+        <div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">HOME</div>
+        <div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">BIO</div>
+        <div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">CONTENT</div>
+        <div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">LINKS</div>
     </div>
 
-    <p>BIO</p>
+    <div id="div-album-release" class="grid grid-cols-1 md:grid-cols-2 mb-10">
+        <div class="p-24">
+            <img src={FallArt} class="object-fit shadow-lg" alt="Another Forest">
+        </div>
+        <div class="grid grid-cols-1 place-items-center content-center text-center">
+            <p class="font-serif text-[50px]">DEBUT EP </p>
+            <p class="font-title text-[80px]">FALL</p>
+            <p class="font-serif text-[50px]"> OUT NOW!</p>
+            <div class="text-lg text-slate-500 w-32 border-green-300 border-solid border-2 rounded-2xl p-5 mt-5 font-serif hover:cursor-pointer hover:bg-green-600 hover:text-white hover:border-green-700 transition-all duration-200">SPOTIFY</div>
+        </div>
+    </div>
 
-    <p>BIO</p>
+    <div class="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi elit, egestas vitae leo sit amet, aliquet consectetur ex. Ut malesuada, nibh in tincidunt tristique, mi elit dignissim dui, non venenatis libero velit sit amet felis. Donec hendrerit dolor et sem volutpat consectetur. Donec vitae vehicula nisi. Cras ut lorem et ligula vestibulum pretium. Mauris tempus ut sem a dignissim. Aliquam dignissim quis tortor a pulvinar. Vivamus bibendum, quam eget congue consectetur, nunc nisi elementum leo, non maximus enim lorem ac erat. Fusce non ex at nibh vehicula maximus. Fusce non ante orci.
+        Nullam finibus posuere tellus, ut finibus diam. Nunc ac ex ut velit rhoncus lacinia. Nulla faucibus tellus sit amet sapien elementum scelerisque. Maecenas vel arcu eget velit pulvinar ornare. Praesent iaculis, diam porttitor rhoncus ullamcorper, massa diam pharetra leo, id ultrices purus nisl a massa. Pellentesque id nulla eu ex vulputate pretium et ac ligula. Etiam nec vestibulum nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris mauris ex, scelerisque at tincidunt iaculis, suscipit ut risus. Sed et nibh nec odio posuere vestibulum. Integer pellentesque pretium odio, vitae convallis leo scelerisque id. Curabitur sed nisl erat.
+        Cras imperdiet nibh sit amet imperdiet sagittis. Pellentesque a vehicula est, tempor ullamcorper felis. Vestibulum ut nibh faucibus, pulvinar nulla ac, venenatis nulla. Donec lobortis neque ac suscipit facilisis. Quisque nec lectus vitae magna pretium rhoncus consequat a tortor. Nam fringilla, tellus eu pretium posuere, sem magna efficitur est, imperdiet ultrices sapien ligula non felis. Morbi eget bibendum odio. Phasellus dignissim finibus quam quis tempor. Integer eu maximus sapien. Sed placerat auctor ipsum, nec aliquam odio pellentesque sed. Aliquam malesuada arcu vitae luctus feugiat. Mauris nisi mauris, aliquet nec pharetra elementum, volutpat vitae felis. In sollicitudin massa ac tellus pulvinar feugiat. Sed efficitur purus risus, ultrices ullamcorper dolor euismod vel.
+    </div>
+
+    <div class="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi elit, egestas vitae leo sit amet, aliquet consectetur ex. Ut malesuada, nibh in tincidunt tristique, mi elit dignissim dui, non venenatis libero velit sit amet felis. Donec hendrerit dolor et sem volutpat consectetur. Donec vitae vehicula nisi. Cras ut lorem et ligula vestibulum pretium. Mauris tempus ut sem a dignissim. Aliquam dignissim quis tortor a pulvinar. Vivamus bibendum, quam eget congue consectetur, nunc nisi elementum leo, non maximus enim lorem ac erat. Fusce non ex at nibh vehicula maximus. Fusce non ante orci.
+        Nullam finibus posuere tellus, ut finibus diam. Nunc ac ex ut velit rhoncus lacinia. Nulla faucibus tellus sit amet sapien elementum scelerisque. Maecenas vel arcu eget velit pulvinar ornare. Praesent iaculis, diam porttitor rhoncus ullamcorper, massa diam pharetra leo, id ultrices purus nisl a massa. Pellentesque id nulla eu ex vulputate pretium et ac ligula. Etiam nec vestibulum nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris mauris ex, scelerisque at tincidunt iaculis, suscipit ut risus. Sed et nibh nec odio posuere vestibulum. Integer pellentesque pretium odio, vitae convallis leo scelerisque id. Curabitur sed nisl erat.
+        Cras imperdiet nibh sit amet imperdiet sagittis. Pellentesque a vehicula est, tempor ullamcorper felis. Vestibulum ut nibh faucibus, pulvinar nulla ac, venenatis nulla. Donec lobortis neque ac suscipit facilisis. Quisque nec lectus vitae magna pretium rhoncus consequat a tortor. Nam fringilla, tellus eu pretium posuere, sem magna efficitur est, imperdiet ultrices sapien ligula non felis. Morbi eget bibendum odio. Phasellus dignissim finibus quam quis tempor. Integer eu maximus sapien. Sed placerat auctor ipsum, nec aliquam odio pellentesque sed. Aliquam malesuada arcu vitae luctus feugiat. Mauris nisi mauris, aliquet nec pharetra elementum, volutpat vitae felis. In sollicitudin massa ac tellus pulvinar feugiat. Sed efficitur purus risus, ultrices ullamcorper dolor euismod vel.
+    </div>
+
+
+    <div class="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi elit, egestas vitae leo sit amet, aliquet consectetur ex. Ut malesuada, nibh in tincidunt tristique, mi elit dignissim dui, non venenatis libero velit sit amet felis. Donec hendrerit dolor et sem volutpat consectetur. Donec vitae vehicula nisi. Cras ut lorem et ligula vestibulum pretium. Mauris tempus ut sem a dignissim. Aliquam dignissim quis tortor a pulvinar. Vivamus bibendum, quam eget congue consectetur, nunc nisi elementum leo, non maximus enim lorem ac erat. Fusce non ex at nibh vehicula maximus. Fusce non ante orci.
+        Nullam finibus posuere tellus, ut finibus diam. Nunc ac ex ut velit rhoncus lacinia. Nulla faucibus tellus sit amet sapien elementum scelerisque. Maecenas vel arcu eget velit pulvinar ornare. Praesent iaculis, diam porttitor rhoncus ullamcorper, massa diam pharetra leo, id ultrices purus nisl a massa. Pellentesque id nulla eu ex vulputate pretium et ac ligula. Etiam nec vestibulum nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris mauris ex, scelerisque at tincidunt iaculis, suscipit ut risus. Sed et nibh nec odio posuere vestibulum. Integer pellentesque pretium odio, vitae convallis leo scelerisque id. Curabitur sed nisl erat.
+        Cras imperdiet nibh sit amet imperdiet sagittis. Pellentesque a vehicula est, tempor ullamcorper felis. Vestibulum ut nibh faucibus, pulvinar nulla ac, venenatis nulla. Donec lobortis neque ac suscipit facilisis. Quisque nec lectus vitae magna pretium rhoncus consequat a tortor. Nam fringilla, tellus eu pretium posuere, sem magna efficitur est, imperdiet ultrices sapien ligula non felis. Morbi eget bibendum odio. Phasellus dignissim finibus quam quis tempor. Integer eu maximus sapien. Sed placerat auctor ipsum, nec aliquam odio pellentesque sed. Aliquam malesuada arcu vitae luctus feugiat. Mauris nisi mauris, aliquet nec pharetra elementum, volutpat vitae felis. In sollicitudin massa ac tellus pulvinar feugiat. Sed efficitur purus risus, ultrices ullamcorper dolor euismod vel.
+    </div>
 
     <div class="text-lg">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi elit, egestas vitae leo sit amet, aliquet consectetur ex. Ut malesuada, nibh in tincidunt tristique, mi elit dignissim dui, non venenatis libero velit sit amet felis. Donec hendrerit dolor et sem volutpat consectetur. Donec vitae vehicula nisi. Cras ut lorem et ligula vestibulum pretium. Mauris tempus ut sem a dignissim. Aliquam dignissim quis tortor a pulvinar. Vivamus bibendum, quam eget congue consectetur, nunc nisi elementum leo, non maximus enim lorem ac erat. Fusce non ex at nibh vehicula maximus. Fusce non ante orci.
