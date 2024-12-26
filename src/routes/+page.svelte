dc433353195ecba62ecfaa5fa905a6c6cba0002a
Thomas Bos <thymer.bos217@gmail.com>
2024-12-27 00:57:00 +0100

    Font changes for small screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
         <p1 class="w-full text-[50px] lg:text-[100px] font-title text-white top-0 left-0 mt-5 absolute">ANOTHER FOREST</p1>
     </div>
 
-    <div id="nav-bar" class="font-nav text-[35px] w-full sticky top-0 bg-white/20 backdrop-blur-sm grid grid-cols-4 content-center place-items-center">
+    <div id="nav-bar" class="font-nav text-[24px] md:text-[35px] w-full sticky top-0 bg-white/20 backdrop-blur-sm grid grid-cols-4 content-center place-items-center">
         <a class="w-full" href="#main"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">HOME</div></a>
         <a class="w-full" href="#div-fall"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">FALL</div></a>
         <a class="w-full" href="#div-bio"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">BIO</div></a>
@@ -36,8 +36,8 @@
     </div>
 
     <div id="div-bio" class="grid grid-cols-1 content-center place-items-center my-10 gap-10">
-        <p class="text-[40px] font-serif text-zinc-500 text-center w-2/3">Who are we?</p>
-        <p class="text-[24px] font-serif text-zinc-700 text-center w-2/3">
+        <p class="text-[32px] md:text-[40px] font-serif text-zinc-500 text-center w-2/3">Who are we?</p>
+        <p class="text-[16px] md:text-[24px] font-serif text-zinc-700 text-center w-2/3">
             Another Forest is a four-piece rock band exploring the boundaries of the genre using their unique mix of female vocals, grinding guitars and catchy choruses. What started for Renske (vocals) and Levi (drums) as a living-room concert on the second floor of a student flat, has grown to be a raging tinnitus machine with the inclusion of Sil (bass) and Thomas (guitar). Their songs take you on an adventure through lush riffs, intense singing, deep bass and powerful drums, yet they are always paired with a whiff of musical ‘je ne sais quoi’. This way, Another Forest aims to reach the top of the music scene and no one will stop them.
         </p>
     </div>
@@ -48,10 +48,10 @@
         Cras imperdiet nibh sit amet imperdiet sagittis. Pellentesque a vehicula est, tempor ullamcorper felis. Vestibulum ut nibh faucibus, pulvinar nulla ac, venenatis nulla. Donec lobortis neque ac suscipit facilisis. Quisque nec lectus vitae magna pretium rhoncus consequat a tortor. Nam fringilla, tellus eu pretium posuere, sem magna efficitur est, imperdiet ultrices sapien ligula non felis. Morbi eget bibendum odio. Phasellus dignissim finibus quam quis tempor. Integer eu maximus sapien. Sed placerat auctor ipsum, nec aliquam odio pellentesque sed. Aliquam malesuada arcu vitae luctus feugiat. Mauris nisi mauris, aliquet nec pharetra elementum, volutpat vitae felis. In sollicitudin massa ac tellus pulvinar feugiat. Sed efficitur purus risus, ultrices ullamcorper dolor euismod vel.
     </div> -->
 
-    <div id="div-links" class="bg-rose-900 text-white p-5 my-10 grid grid-cols-3 content-center place-items-center text-center">
-        <div><i class="fa-brands fa-spotify text-[50px]"></i></div>
-        <div><i class="fa-brands fa-instagram text-[50px]"></i></div>
-        <div><i class="fa-brands fa-tiktok text-[50px]"></i></div>
+    <div id="div-links" class="bg-rose-900 text-white my-10 grid grid-cols-3 content-center place-items-center text-center">
+        <a class="w-full h-full hover:bg-rose-950 p-5" target="_blank" href="https://open.spotify.com/artist/0YXSigMGqfDvBAtzqED2Zs?si=BrwbqRsZQ6mnvVcc0Q-olg"><i class="fa-brands fa-spotify text-[50px]"></i></a>
+        <a class="w-full h-full hover:bg-rose-950 p-5" target="_blank" href="https://www.instagram.com/anotherforestband/"><i class="fa-brands fa-instagram text-[50px]"></i></a>
+        <a class="w-full h-full hover:bg-rose-950 p-5" target="_blank" href="https://www.tiktok.com"><i class="fa-brands fa-tiktok text-[50px]"></i></a>
     </div>
 
     <div id="div-footer" class="mt-10 bg-rose-950 text-white grid grid-cols-1 md:grid-cols-2 place-items-center content-center text-center">
