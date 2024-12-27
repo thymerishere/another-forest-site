85e6a2a9c4d3942e59a4733a545af0055019649b
Thomas Bos <thymer.bos217@gmail.com>
2024-12-27 02:21:02 +0100

    Add logo to footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,10 @@
+<script>
+    import Logo from "$lib/images/logo-transparent.png"
+</script>
 
-<div id="div-footer" class="relative pt-10 bg-rose-950 text-white grid grid-cols-1 md:grid-cols-2 place-items-center content-center text-center">
-    <p class="m-5">This website has been made by Another Forest</p>
-    <p class="m-5">(Spread the word!)</p>
+<div id="div-footer" class="relative pt-10 pb-5 bg-rose-950 text-white grid grid-cols-1 place-items-center">
+    <div class="w-3/5 grid grid-cols-1 place-items-center content-center text-center">
+        <p class="m-5 font-serif">This website has been made by Another Forest</p>
+        <img src={Logo} class="w-[50px]" alt="Logo">
+    </div>
 </div>
