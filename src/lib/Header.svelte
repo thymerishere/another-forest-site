133d2f9f2d6bc4e15dbaab22aac46c69f70d5b01
Thomas Bos <thymer.bos217@gmail.com>
2024-12-27 02:11:41 +0100

    Add logo

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,17 @@
 <script>
     import HeaderImage from "$lib/images/brick-wall-bw.jpg"
+    import Logo from "$lib/images/logo-transparent.png"
 </script>
 
 <div id="div-header" class="overflow-hidden z-0">
     <img src={HeaderImage} class="fixed h-[90vh] w-full object-cover" alt="Another Forest">
-    <div id="header-div" class="h-[90vh] w-full text-center">
-        <!-- <img src={HeaderImage} class="h-full w-full object-cover" alt="Another Forest"> -->
-        <p1 class="w-full text-[100px] md:text-[100px] lg:text-[150px] top-[45vh] md:top-[60vh] font-title text-white left-0 mt-5 absolute">ANOTHER FOREST</p1>
+
+    <div id="header-div" class="h-[90vh] w-full">
+        <div class="absolute top-[20vh] w-full grid grid-cols-1 place-items-center">
+            <img src={Logo} class="h-[100px] xl:h-[150px]" alt="Logo">
+        </div>
+        <div class="absolute top-[10vh] md:top-[20vh] w-full grid grid-cols-1 place-items-center text-center">
+            <p1 class="absolute top-[30vh] text-[100px] xl:text-[150px] font-title text-white">ANOTHER FOREST</p1>
+        </div>
     </div>
 </div>
