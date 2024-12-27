779b33da1764e206eed1db43013b6eb5700f1331
Thomas Bos <thymer.bos217@gmail.com>
2024-12-27 12:20:29 +0100

    Remove shadow from nav

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,6 +1,6 @@
 <div id="nav-bar" class="sticky z-10 font-nav text-[24px] md:text-[35px] w-full top-0 bg-white/80 backdrop-blur-sm grid grid-cols-4 content-center place-items-center">
-    <a class="w-full" href="#main"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">HOME</div></a>
-    <a class="w-full" href="#div-fall"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">FALL</div></a>
-    <a class="w-full" href="#div-bio"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">BIO</div></a>
-    <a class="w-full" href="#div-links"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:shadow-lg hover:bg-rose-950 hover:text-white transition-all duration-200">LINKS</div></a>
+    <a class="w-full" href="#main"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:bg-rose-950 hover:text-white transition-all duration-200">HOME</div></a>
+    <a class="w-full" href="#div-fall"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:bg-rose-950 hover:text-white transition-all duration-200">FALL</div></a>
+    <a class="w-full" href="#div-bio"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:bg-rose-950 hover:text-white transition-all duration-200">BIO</div></a>
+    <a class="w-full" href="#div-links"><div class="content-center text-center h-[10vh] w-full hover:cursor-pointer hover:bg-rose-950 hover:text-white transition-all duration-200">LINKS</div></a>
 </div>
