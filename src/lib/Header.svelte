82fc815b29c29b029de46eb2f748a8f627a7bb0b
Thomas Bos <thymer.bos217@gmail.com>
2024-12-27 01:46:58 +0100

    Fix some text sizes on small screens

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,6 +6,6 @@
     <img src={HeaderImage} class="fixed h-[90vh] w-full object-cover" alt="Another Forest">
     <div id="header-div" class="h-[90vh] w-full text-center">
         <!-- <img src={HeaderImage} class="h-full w-full object-cover" alt="Another Forest"> -->
-        <p1 class="w-full text-[50px] lg:text-[100px] font-title text-white top-0 left-0 mt-5 absolute">ANOTHER FOREST</p1>
+        <p1 class="w-full text-[100px] md:text-[100px] lg:text-[150px] top-[45vh] md:top-[60vh] font-title text-white left-0 mt-5 absolute">ANOTHER FOREST</p1>
     </div>
 </div>
